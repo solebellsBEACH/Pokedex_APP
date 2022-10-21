e0156d2b6fb703fb4d50652da422131eaa78a203
solebellsBEACH <lucassxxavier@gmail.com>
2022-10-21 11:34:29 -0300

    terminando função de buscar por nome

--- src/screens/Home/useHome/components/PokemonInput/index.tsx
+++ src/screens/Home/useHome/components/PokemonInput/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { Container } from './styles'
+import { Creators as HomeActions } from '../../../../../store/ducks/home'
+import { SafeAreaView } from 'react-native'
 
-export const PokemonInput = () => {
+export const PokemonInput = (props: any) => {
+    const [state, setState] = useState<string>('')
+    const dispatch = useDispatch()
+    const onSubmit = (): void => {
+        dispatch(HomeActions.HomePokemonsRequest({
+            offset: 0,
+            limit: 20,
+            name:state.toLowerCase()
+        }))
+    }
+
     return (
+
         <Container
+            onSubmitEditing={onSubmit}
+            value={state}
+            onChangeText={e => setState(e)}
             placeholder='Buscar Pokémon'
         >
         </Container>
+
     )
 }
