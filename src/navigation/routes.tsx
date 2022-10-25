e118989e466b92f4680755b2006bdad90e147bfa
solebellsBEACH <lucassxxavier@gmail.com>
2022-10-25 20:19:00 -0300

    config Routes

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,43 +1,23 @@
-import React from 'react';
-
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BattleScreen, PokemonScreen } from '../screens';
+import { TabNavigator } from './TabNavigator';
 
-import { LogIn, Home, PokemonScreen, PokemonBattle, BattleScreen } from '../screens';
-import theme from '../global/theme';
-import { RFValue } from 'react-native-responsive-fontsize';
-
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function Routes() {
-    return (<>
+    return (
         <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName='PokemonBattle'
-                screenOptions={{
-                    headerShown: false, tabBarShowLabel: false, tabBarStyle: {
-                        positon: 'absolute',
-                        height: RFValue(60),
-                        backgroundColor: theme.colors.gray2,
-                        bottom: RFValue(10),
-                        width: '92%',
-                        left: '4%',
-                        elevation:0,
-                        borderRadius:RFValue(15)
-                    }
-                }}
+            <Stack.Navigator
+              screenOptions={{
+                headerShown: false,}}
             >
-                <Tab.Screen name="LogIn"
-                    component={LogIn} />
-                <Tab.Screen name="Home"
-                    component={Home} />
-                <Tab.Screen name="PokemonScreen"
+                <Stack.Screen name="HomePage" component={TabNavigator} />
+                <Stack.Screen name="PokemonScreen"
                     component={PokemonScreen} />
-                <Tab.Screen name="PokemonBattle"
-                    component={PokemonBattle} />
-                <Tab.Screen name="BattleScreen"
+                <Stack.Screen name="BattleScreen"
                     component={BattleScreen} />
-            </Tab.Navigator>
+            </Stack.Navigator>
         </NavigationContainer>
-    </>)
+    );
 }
